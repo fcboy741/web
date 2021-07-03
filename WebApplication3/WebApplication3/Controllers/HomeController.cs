19cf7b3af12b79cc41f89cad17584702d81dea8e
Trần Đăng Khoa <71996922+fcboy741@users.noreply.github.com>
2021-07-03 11:32:11 +0700

    code

---
+++ WebApplication3/WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Men()
+        {
+            return View();
+        }
+        public IActionResult Women()
+        {
+            return View();
+        }
+        public IActionResult Kids()
+        {
+            return View();
+        }
+        public IActionResult Sports()
+        {
+            return View();
+        }
+        public IActionResult NikeSportswear()
+        {
+            return View();
+        }
+        public IActionResult NikeID()
+        {
+            return View();
+        }
+    }
+}
